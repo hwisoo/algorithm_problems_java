07edb9613d876e90808871f2ff5fdcf363ac5eb1
James Cho <hwisoocho@gmail.com>
2020-05-10 15:39:44 -0500

    solution with array.sort

--- src/Findpair.java
+++ src/Findpair.java
@@ -1,19 +1,45 @@
+import java.util.Arrays;
+
 public class Findpair {
     // Given an unsorted array of integers, find a pair with given sum in it.
     public static void findPair(int[] Arr, int sum) {
 
         // Nested loops - considers every pair in given array and return if desired sum is found.
-        System.out.println("Sum is " + sum);
-        for (int i = 0; i < Arr.length; i++) {
-            System.out.println("i is " + Arr[i]);
-            for (int j = i + 1; j <Arr.length; j++) {
-                System.out.println("j is " + Arr[j]);
-                if (Arr[i] + Arr[j] == sum) {
-                    System.out.println(Arr[i] + " + " + Arr[j] + " == " + sum);
-                    return;
-                }
-            }
-        }
+//        System.out.println("Sum is " + sum);
+//        for (int i = 0; i < Arr.length; i++) {
+//            System.out.println("i is " + Arr[i]);
+//            for (int j = i + 1; j <Arr.length; j++) {
+//                System.out.println("j is " + Arr[j]);
+//                if (Arr[i] + Arr[j] == sum) {
+//                    System.out.println(Arr[i] + " + " + Arr[j] + " == " + sum);
+//                    return;
+//                }
+//            }
+//        }
+
+//         No pair with given sum exists
+//        System.out.println("Pair not found");
+
+        // Solution using sorting - more efficient
+        Arrays.sort(Arr);
+         int low = 0;
+         int high = Arr.length -1;
+
+         while (low < high) {
+             if (Arr[low] + Arr[high] == sum) {
+                 System.out.println(Arr[low] + " + " + Arr[high] + " == " + sum);
+                 return;
+             }
+
+             if (Arr[low] + Arr[high] < sum) {
+                 low++;
+             } else {
+                 high--;
+             }
+         }
+    //  No pair with given sum exists
+        System.out.println("Pair not found");
+
     }
 
     // main function
