e9a0f5b2a81fed783d3ea98f1bc48a8214c02389
James Cho <hwisoocho@gmail.com>
2020-05-14 15:19:55 -0500

    count number of zeros and fill the array accordingly

---
+++ src/SortBinaryArray.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+
+public class SortBinaryArray {
+
+    public static void sort(int[] A) {
+        int zeros = 0;
+        for (int i = 0; i < A.length; i++ ) {
+            if (A[i] == 0) {
+                zeros++;
+            }
+        }
+        System.out.println("number of zeros = " + zeros);
+
+        // put 0's in the beginning
+        int k = 0;
+        while (zeros-- != 0) {
+            A[k++] = 0;
+        }
+
+        // fill all remaining elements by 1
+        while (k < A.length) {
+            A[k++] = 1;
+        }
+    }
+
+    // Sort binary array in linear time
+    public static void main(String[] arg) {
+        int A[] = { 0, 0, 1, 0, 1, 1, 0, 1, 0, 0};
+        sort(A);
+
+        // print the rearranged array
+        System.out.println(Arrays.toString(A));
+    }
+}
